08d247b6753c640543edf1a4b5c21319febfda72
oybekmuzropov <muzropov.oybek@gmail.com>
2020-05-25 15:11:52 +0500

    added category,measure endpoints

--- api/handlers/v1/category.go
+++ api/handlers/v1/category.go
@@ -12,6 +12,16 @@ import (
 	"net/http"
 )
 
+// @Router /v1/category [post]
+// @Summary Create Category
+// @Description API for creating category
+// @Tags category
+// @Accept  json
+// @Produce  json
+// @Param category body models.CreateCategoryModel true "category"
+// @Success 201 {object} models.Response
+// @Failure 400 {object} models.ResponseError
+// @Failure 500 {object} models.ResponseError
 func (h *handlerV1) CreateCategory(c *gin.Context) {
 	var (
 		unmarshal jsonpb.Unmarshaler
@@ -42,10 +52,20 @@ func (h *handlerV1) CreateCategory(c *gin.Context) {
 	c.JSON(http.StatusCreated, resp)
 }
 
+// @Router /v1/category [get]
+// @Summary Get All Category
+// @Description API for getting all category
+// @Tags category
+// @Accept  json
+// @Produce  json
+// @Param page query integer false "page"
+// @Success 200 {object} models.GetAllCategoriesModel
+// @Failure 400 {object} models.ResponseError
+// @Failure 500 {object} models.ResponseError
 func (h *handlerV1) GetAllCategory(c *gin.Context) {
 	var (
 		marshaler jsonpb.Marshaler
-		model models.GetAllCategory
+		model models.GetAllCategoriesModel
 	)
 	page, err := ParsePageQueryParam(c)
 
@@ -74,7 +94,17 @@ func (h *handlerV1) GetAllCategory(c *gin.Context) {
 	fmt.Println(js)
 
 	err = json.Unmarshal([]byte(js), &model)
-	//model.Count = resp.Count
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, models.ResponseError{
+			Error: models.InternalServerError {
+				Code:ErrorCodeInternal,
+				Message:"error while parsing proto to struct",
+			},
+		})
+		h.log.Error("error while parsing proto to struct", logger.Error(err))
+		return
+	}
 
 	c.JSON(http.StatusOK, model)
 }
--- api/models/category.go
+++ api/models/category.go
@@ -1,6 +1,13 @@
 package models
 
-type Category struct {
+type CreateCategoryModel struct {
+	Name string `json:"name"`
+	ParentID string `json:"parent_id"`
+	Description string `json:"description"`
+	OrderNo int64 `json:"order_no,string"`
+}
+
+type GetCategoryModel struct {
 	ID string `json:"id"`
 	Name string `json:"name"`
 	Slug string `json:"slug"`
@@ -8,7 +15,7 @@ type Category struct {
 	Description string `json:"description"`
 }
 
-type GetAllCategory struct {
-	Categories []Category `json:"categories"`
+type GetAllCategoriesModel struct {
+	Categories []GetCategoryModel `json:"categories"`
 	Count int64 `json:"count,string"`
 } 
