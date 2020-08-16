c5879852120ab8eb76e6bdb7e356c20db80d7278
temur_mannonov <t.mannonov@gmail>
2020-08-16 19:18:14 +0500

    add order no for catalgos

--- api/models/category.go
+++ api/models/category.go
@@ -1,24 +1,25 @@
 package models
 
 type CreateCategoryModel struct {
-	Name string `json:"name"`
-	ParentID string `json:"parent_id"`
+	Name        string `json:"name"`
+	ParentID    string `json:"parent_id"`
 	Description string `json:"description"`
-	OrderNo int64 `json:"order_no,string"`
-	Image string `json:"image"`
+	OrderNo     int64  `json:"order_no"`
+	Image       string `json:"image"`
 }
 
 type GetCategoryModel struct {
-	ID string `json:"id"`
-	Name string `json:"name"`
-	Slug string `json:"slug"`
-	ParentID string `json:"parent_id"`
-	Description string `json:"description"`
+	ID              string             `json:"id"`
+	Name            string             `json:"name"`
+	Slug            string             `json:"slug"`
+	ParentID        string             `json:"parent_id"`
+	Description     string             `json:"description"`
 	ChildCategories []GetCategoryModel `json:"child_categories"`
-	Image string `json:"image"`
+	Image           string             `json:"image"`
+	OrderNo         int64              `json:"order_no"`
 }
 
 type GetAllCategoriesModel struct {
 	Categories []GetCategoryModel `json:"categories"`
-	Count int64 `json:"count,string"`
-} 
+	Count      int64              `json:"count,string"`
+}
